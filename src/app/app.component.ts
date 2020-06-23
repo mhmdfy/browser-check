3c143b9d6070f8768c0efad1f964e4b355a7d192
Momo <mhmdfy@gmail.com>
2020-06-23 13:28:38 +0300

    working initially

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'browser-check';
+
+  constructor(private deviceService: DeviceDetectorService) {}
+
+  ngOnInit() {
+    console.log("Is browser supported?", this.supportedBrowser());
+    if (this.supportedBrowser()) {
+      window.location.href = 'https://meet.aramco.com/';
+    }
+  }
+
+  supportedBrowser() : boolean {
+    let ua = window.navigator.userAgent;
+    console.log('user agent', ua);
+
+    if (this.deviceService.isDesktop) {
+      return (this.deviceService.browser == 'Chrome' || this.deviceService.browser == 'Firefox' || this.deviceService.browser == 'MS-Edge-Chromium')
+    }
+
+    // return ua.indexOf('Chrome') !== -1 || ua.indexOf('Firefox') !== -1;
+    return false;
+  }
+
 }
